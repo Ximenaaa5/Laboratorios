bdd97a5bced878f0f98743e331a7df59008d0e9d
Ximenaaa5 <158371441+Ximenaaa5@users.noreply.github.com>
2024-05-15 11:30:44 -0600

    Add files via upload

---
+++ Semana 10/T10, PXAR, 1287424, act 1 terminado.py
@@ -0,0 +1,149 @@
+print ("Semana No. 10: Ejercicio 1")
+mes=int(input("Ingrese un número entre 1 y 12:"))
+if mes<1 or mes>12:
+    print("Error: el número debe de estar entre 1 y 12")
+else:
+    if mes==1:
+     print("Mes: enero")
+    elif mes==2:
+     print ("Mes: febrero")
+    elif mes==3:
+     print ("Mes: marzo")
+    elif mes==4:
+      print("Mes: abril")
+    elif mes==5:
+      print("Mes: mayo")
+    elif mes==6:
+      print("Mes: junio")
+    elif mes==7:
+      print("Mes:julio")
+    elif mes==8:
+      print("Mes: agosto")
+    elif mes==9:
+      print("Mes: septiembre")
+    elif mes==10:
+      print("Mes: octubre")
+    elif mes==11:
+      print("Mes: noviembre")
+    elif mes==12:
+      print("Mes: diciembre")
+
+
+print()
+print("Semana10: Ejercicio 2")
+
+A=int(input("Ingrese el primer número:"))
+B=int(input("Ingrese su segundo número:"))
+C=int(input("Ingrese su tercer número:"))
+
+if A>B:
+    if A>C:
+        print("El mayor es:",A)
+    else:    
+        if A==C:
+            print("Los mayores son:",A, "y", C)
+        else:    
+            print("El mayor es:",C)
+else:
+    if A==B:
+        if A>C:
+            print ("Los mayores son: ",A, "y" ,B)
+        else:
+            if A==C:
+                print( "Los tres valores son iguales")
+            else:    
+                print ("El mayor es:",C)
+    else:
+        if B>C:
+            print("El mayor es:",B)
+        else:
+            if B==C:
+                print("Los mayores son: " ,B, "y" ,C)
+            else:
+                print("El mayor es:",C)
+
+
+ if mes==1:
+ if día in range(20,32):
+    print("Tu signo zodiacal es: Acuario")
+if mes==2:
+  if día in range (1,19):
+    print("Tu signo zodiacal es: Acuario")
+if mes ==2:
+   if día in range (19,29):
+      print("Tu signo zodiacal es: Piscis")
+if mes ==3:
+   if día in range (1,21):
+      print("Tu signo zodiacal es: Piscis")
+if mes == 3:
+   if día in range (21,32):
+      print("Tu signo zodiacal es: Aries")
+if mes==4:
+   if día in range (1,20):
+      print("Tu signo zodiacal es: Aries")
+if mes==4:
+   if día in range(20,31):
+      print("Tu signo zodiacal es: Tauro")
+if mes==5:
+   if día in range (1,21):
+      print("Tu signo zodiacal es: Tauro")
+if mes==5:
+   if día in range(21,32):
+      print("Tu signo zodiacal es: Géminis")
+if mes==6:
+   if día in range (1,21):
+      print("Tu signo zodiacal es: Géminis")
+if mes==6:
+   if día in range(21,31):
+      print("Tu signo zodiacal es: Cancer")
+if mes==7:
+   if día in range(1,23):
+      print("Tu signo zodiacal es: Cancer")
+if mes==7:
+   if día in range(23,32):
+      print("Tu signo zodiacal es: Leo")
+if mes==8:
+   if día in range (1,23):
+      print("Tu signo zodiacal es: Leo")
+if mes==8:
+   if día in range(23,32):
+      print("Tu signo zodiacal es: Virgo")
+if mes==9:
+   if día in range(1,23):
+      print("Tu signo zodiacal es: Virgo")
+if mes==9:
+   if día in range(23,31):
+      print("Tu signo zodiacal es: Libra")
+if mes==10:
+   if día in range(1,23):
+      print("Tu signo zodiacal es: Libra")
+if mes==10:
+   if día in range (23,32):
+      print("Tu signo zodiacal es: Escorpio")
+if mes==11:
+   if día in range(1,22):
+      print("Tu signo zodiacal es: Escorpio")
+if mes==11:
+   if día in range(22,31):
+      print("Tu signo zodiacal es: Sagitario")
+if mes==12:
+   if día in range(1,22):
+      print("Tu signo zodiacal es: Sagitario")
+if mes==12:
+   if día	in range(22,32):
+      print("Tu signo zodiacal es: Capricornio")
+if mes==1:
+   if día in range(1,20):
+       print("Tu signo zodiacal es: Capricornio")
+if mes==1:
+ if día in range(20,32):
+    print("Tu signo zodiacal es: Acuario")
+if mes==2:
+  if día in range (1,19):
+    print("Tu signo zodiacal es: Acuario")
+if mes ==2:
+   if día in range (19,29):
+      print("Tu signo zodiacal es: Piscis")
+if mes ==3:
+   if día in range (1,21):
+      print("Tu signo zodiacal es: Piscis")               
---
+++ Semana 9/T9, pxar, 1287424.py
@@ -0,0 +1,14 @@
+
+n1 = float(input(" Ingrese Cantidad: "))
+
+millas = n1 / 1609.34
+kilometros = n1 / 1000
+pies = n1 * 3.28084
+pulgadas = pies * 12
+yardas = pies / 3
+
+print("Cantidad en millas: "+str(millas)+"millas")
+print("Cantidad en kilometros: "+str(kilometros)+"kilometros")
+print("Cantidad en pies: "+str(pies)+"pies")
+print("Cantidad en pulgadas: "+str(pulgadas)+"pulgadas")
+print("Cantidad en yardas: "+str(yardas)+"yardas")
